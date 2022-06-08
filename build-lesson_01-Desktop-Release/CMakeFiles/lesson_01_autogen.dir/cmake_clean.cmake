eea443edeb17ccd21059df46495e5f98596215e4
Anton Protopopov <antpro@basealt.ru>
2022-06-08 13:08:18 +0400

    fix: refactoring folder

--- build-lesson_01-Desktop-Release/CMakeFiles/lesson_01_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/lesson_01_autogen"
-  "lesson_01_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/lesson_01_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
