f21126a4560316ac5bb40880de980aa8cc4f1169
Alexey Saprunov <workernight@yandex.ru>
2022-07-02 12:52:15 +0400

    qt snake

---
+++ AlexSP0/qt_snake/main.cpp
@@ -0,0 +1,16 @@
+#include "mainwindow.h"
+#include "snake.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    Snake window;
+
+    window.setWindowTitle("Snake");
+    window.show();
+
+    return a.exec();
+}
