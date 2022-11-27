907234cffc9cac7fdaa82eadc11513b01d70fdbe
Alexey Saprunov <workernight@yandex.ru>
2022-11-27 22:46:12 +0400

    feat: show notification via dbus

--- AlexSP0/qtdbus/mainwindow.cpp
+++ AlexSP0/qtdbus/mainwindow.cpp
@@ -1,6 +1,12 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 
+#include <QtDBus/QDBusConnection>
+#include <QtDBus/QDBusConnectionInterface>
+#include <QtDBus/QDBusInterface>
+#include <QtDBus/QDBusMessage>
+#include <QtDBus/QDBusReply>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -13,3 +19,36 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::on_show_clicked()
+{
+    if (!ui->lineEditHeader->text().isEmpty() && !ui->textEdit->toPlainText().isEmpty())
+    {
+        showNotification(ui->lineEditHeader->text(), ui->textEdit->toPlainText());
+    }
+    else
+    {
+        showNotification("ERROR!", "Some fields are empty!");
+    }
+}
+
+void MainWindow::showNotification(QString header, QString body)
+{
+    QDBusConnection bus = QDBusConnection::sessionBus();
+    QDBusInterface dbus_iface("org.freedesktop.Notifications",
+                              "/org/freedesktop/Notifications",
+                              "org.freedesktop.Notifications",
+                              bus);
+
+    QList<QVariant> arguments;
+
+    arguments.append("qtdbus");
+    arguments.append(QVariant::UInt);
+    arguments.append("");
+    arguments.append(header);
+    arguments.append(body);
+    arguments.append(QStringList());
+    arguments.append(QVariantMap());
+    arguments.append(20000);
+
+    auto answer = dbus_iface.callWithArgumentList(QDBus::AutoDetect, "Notify", arguments);
+}
--- AlexSP0/qtdbus/mainwindow.h
+++ AlexSP0/qtdbus/mainwindow.h
@@ -4,7 +4,10 @@
 #include <QMainWindow>
 
 QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
+namespace Ui
+{
+class MainWindow;
+}
 QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
@@ -15,7 +18,13 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_show_clicked();
+
 private:
     Ui::MainWindow *ui;
+
+private:
+    void showNotification(QString header, QString body);
 };
 #endif // MAINWINDOW_H
